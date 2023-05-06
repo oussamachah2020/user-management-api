cdb82782807bb37590dbb8beeae5d45f05edbc7e
oussamachah2020 <otaksam213@gmail.com>
2023-05-07 00:42:34 +0100

    WIP

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,7 +1,15 @@
 from pydantic import BaseModel
+from bson import objectid
 
 
-class User(BaseModel):
+class registrationModel(BaseModel):
+    _id: objectid
     username: str
+    email: str
+    password: str
+
+
+class loginModel(BaseModel):
+    _id: objectid
     email: str
     password: str
--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter, Response
 from config.settings import settings
-from models.user_model import User
+from models.user_model import registrationModel, loginModel
 from config.db import db
 import bcrypt
+import jwt
 
 userRouter = APIRouter()
 
 
 @userRouter.post("/create")
-async def create_user(user: User):
+async def create_user(user: registrationModel):
     existed_user = db.users.find_one({"email": user.email})
 
     if existed_user:
@@ -21,7 +22,30 @@ async def create_user(user: User):
 
     newUser = db.users.insert_one(user.dict())
 
+    encoded_jwt = jwt.encode({newUser._id: "payload"},
+                             "secret", algorithm="HS256")
+
     if newUser:
-        return Response(status_code=201, content=["user created successfully", {"user": user}])
+        return Response(status_code=201, content=["user created successfully", {"token": encoded_jwt}])
     else:
         return Response(status_code=500)
+
+
+@userRouter.get("/all")
+async def get_user(user: loginModel):
+    searched_user = db.users.find({"email": user.email})
+    # user = searched_user.
+    encoded_jwt = jwt.encode({user._id: "payload"},
+                             settings.JWT_PRIVATE_KEY, algorithm="HS256")
+
+    user_password_encoded = user.password.encode("utf-8")
+    passwords_matching = bcrypt.checkpw(
+        user_password_encoded, searched_user.password)
+
+    if searched_user:
+        if passwords_matching:
+            return Response(status_code=200, content={"token": encoded_jwt})
+        else:
+            return Response(status_code=400, content="Incorrect password")
+    else:
+        return Response(status_code=404, content="user not found")
